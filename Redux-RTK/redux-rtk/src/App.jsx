dede70ac2c1d089b00dfcd99f201f1f9879a08d1
AnupKamble <sahyadrihillsagro@gmail.com>
2025-06-07 19:15:59 +0530

    css applied

--- Redux-RTK/redux-rtk/src/App.jsx
+++ Redux-RTK/redux-rtk/src/App.jsx
@@ -2,11 +2,32 @@ import './App.css'
 import Counter from './app/features/counter/Counter'
 
 function App() {
+  const titleStyle = {
+    fontSize: '28px',
+    fontWeight: 'bold',
+    marginBottom: '20px',
+    textAlign: 'center',
+    color: '#333',
+  };
+
+  const containerStyle = {
+    textAlign: 'center',
+    padding: '40px',
+    border: '1px solid #ccc',
+    borderRadius: '8px',
+    maxWidth: '400px',
+    margin: '50px auto',
+    boxShadow: '0 2px 10px rgba(0, 0, 0, 0.1)',
+    backgroundColor: '#f9f9f9',
+  };
+
  return (
     <>
-
-    <h1>Redux Toolkit implementation </h1>  
-      <Counter /> 
+   <div style={containerStyle} >
+   <h1 style={titleStyle}>Redux Toolkit Counter </h1>  
+   <Counter /> 
+   </div>
+    
         
       </>
   )
--- Redux-RTK/redux-rtk/src/app/features/counter/Counter.jsx
+++ Redux-RTK/redux-rtk/src/app/features/counter/Counter.jsx
@@ -4,19 +4,41 @@ import { useSelector, useDispatch } from 'react-redux';
 import { increment, decrement} from './counterSlice';
 
 const Counter = () => {
-  const countVal = useSelector(incr => incr.counter.value);
 
-  console.log(countVal)
+  const decrbuttonStyle = {
+    margin: '5px',
+    padding: '10px 20px',
+    fontSize: '16px',
+    backgroundColor: 'red',
+    color: 'white',
+    border: 'none',
+    borderRadius: '6px',
+    cursor: 'pointer',
+  };
+  const incrbuttonStyle = {
+    margin: '5px',
+    padding: '10px 20px',
+    fontSize: '16px',
+    backgroundColor: '#4CAF50',
+    color: 'white',
+    border: 'none',
+    borderRadius: '6px',
+    cursor: 'pointer',
+  };
+
+
+  const countVal = useSelector(incr => incr.counter.value); //  Accissing the value from the state using useSelector hook
+                              // var = var.reducer.initialState.value
+
+  // console.log(countVal)
   const dispatch = useDispatch();
 
   return (
     <div>
-     
-
-      <button onClick={() => dispatch(decrement())} >-</button>
-      <button onClick={() => dispatch(increment())}>+</button>
+      <button style={decrbuttonStyle} onClick={() => dispatch(decrement())} >-</button>
+      <button style={incrbuttonStyle} onClick={() => dispatch(increment())}>+</button>
       <h2>Count: {countVal}</h2>
-
+ 
     </div>
   );
 };
